f7e016aeddfb7b094577f402c65b7c4c693393af
pontaGC <makizawawa@gmail.com>
2024-01-05 01:15:21 +0900

    Create DoubleHelper.

---
+++ CSharp/SimpleCalculator.Core/Utils/DoubleHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace SimpleCalculator.Core.Utils
+{
+    /// <summary>
+    /// The utility class for the double struct type.
+    /// </summary>
+    public static class DoubleHelper
+    {
+        private const NumberStyles DefaultNumberStyle = NumberStyles.AllowThousands | NumberStyles.Float;
+
+        /// <summary>
+        /// Try to convert the given string to double value.
+        /// </summary>
+        /// <param name="input">The string to covnert.</param>
+        /// <param name="result">The double value after converting.</param>
+        /// <returns>
+        /// <c>true</c> if converting <paramref name="input"/> to the double value is successful,
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public static bool TryParseInvariantCulture(string input, out double result)
+        {
+            return double.TryParse(
+                input,
+                DefaultNumberStyle,
+                CultureInfo.InvariantCulture,
+                out result);
+        }
+    }
+}
