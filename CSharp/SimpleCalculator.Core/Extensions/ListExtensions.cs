7a3c41884b281fa760062b1492ee3964d8860416
pontaGC <makizawawa@gmail.com>
2024-01-14 13:52:02 +0900

    Unused Extension methods

--- CSharp/SimpleCalculator.Core/Extensions/ListExtensions.cs
+++ CSharp/SimpleCalculator.Core/Extensions/ListExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleCalculator.Core.Extensions
+﻿using System.Diagnostics;
+
+namespace SimpleCalculator.Core.Extensions
 {
     /// <summary>
     /// Extension methods for List type.
@@ -6,53 +8,14 @@
     public static class ListExtensions
     {
         /// <summary>
-        /// Removes the last occurrence of a specific object from the <see cref="IList{T}"/>.
-        /// </summary>
-        /// <typeparam name="T">The type of elements in the list.</typeparam>
-        /// <param name="source">The source.</param>
-        /// <param name="item">The object to remove from the <see cref="IList{T}"/>. The value can be <c>null</c> for reference types.</param>
-        /// <returns>
-        /// The <c>true</c> if <c>item</c> is successfully removed; otherwise, <c>false</c>. This method also returns <c>false</c>
-        /// if <c>item</c> was not found in the <see cref="IList{T}"/>.
-        /// </returns>
-        public static bool RemoveLast<T>(this IList<T> source, T item)
-        {
-            if (source is null || !source.Any())
-            {
-                return false;
-            }
-
-            if (source.IsReadOnly)
-            {
-                return false;
-            }
-
-            var lastIndex = source.IndexOf(item);
-            if (lastIndex < 0)
-            {
-                // Not found item
-                return false;
-            }
-
-            try
-            {
-                source.RemoveAt(lastIndex);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
         /// Removes the last element of a specific object from the <see cref="IList{T}"/>.
         /// </summary>
         /// <typeparam name="T">The type of elements in the list.</typeparam>
         /// <param name="source">The source.</param>
         /// <returns>
         /// The <c>true</c> if the last element is successfully removed; otherwise, <c>false</c>.
-        /// </returns>
+        /// </returns>d
+        [DebuggerStepThrough]
         public static bool RemoveLast<T>(this IList<T> source)
         {
             if (source is null || !source.Any())
--- CSharp/SimpleCalculator.Core/Extensions/StringExtensions.cs
+++ CSharp/SimpleCalculator.Core/Extensions/StringExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace SimpleCalculator.Core.Extensions
@@ -9,66 +8,6 @@ namespace SimpleCalculator.Core.Extensions
     /// </summary>
     public static class StringExtension
     {
-        /// <summary>
-        /// Gets a last character in the given string.
-        /// </summary>
-        /// <param name="source">The string to get.</param>
-        /// <returns>
-        /// An empty string if <c>source</c> is <c>null</c> or an empty string,
-        /// otherwise, returns the last character.
-        /// </returns>
-        [DebuggerStepThrough]
-        [return: NotNull]
-        public static string GetLastCharacter(this string source)
-        {
-            if (string.IsNullOrEmpty(source))
-            {
-                return string.Empty;
-            }
-
-            return source.Last().ToString();
-        }
-
-        /// <summary>
-        /// Removes one target string at end of the given string.
-        /// </summary>
-        /// <param name="source">The source string to remove.</param>
-        /// <param name="removeString">The removing target string.</param>
-        /// <returns>A new string after removing the <paramref name="removeString"/> at end.</returns>
-        [return: NotNull]
-        public static string RemoveEndOnce(this string source, string removeString)
-        {
-            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(removeString))
-            {
-                return string.Empty;
-            }
-
-            var lastIndex = source.LastIndexOf(removeString);
-            if (lastIndex < 0)
-            {
-                // Not found the removing target string
-                return source;
-            }
-
-            return source.Remove(lastIndex, removeString.Length);
-        }
-
-        /// <summary>
-        /// Removes all whitespaces in the target string.
-        /// </summary>
-        /// <param name="target">The target string.</param>
-        /// <returns>The string with all whitespaces removed.</returns>
-        [return: NotNull]
-        public static string RemoveAllWhitespaces(this string target)
-        {
-            if (string.IsNullOrEmpty(target))
-            {
-                return string.Empty;
-            }
-
-            return Regex.Replace(target, "\\s", string.Empty);
-        }
-
         /// <summary>
         /// Splits the given string with whitespace.
         /// </summary>
